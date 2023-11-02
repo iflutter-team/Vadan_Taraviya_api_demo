dd4b1683be6e107db0e9fc370b344d9a202e3885
Vandan Taraviya <vandantaraviya94@gmail.com>
2023-11-02 17:01:54 +0530

    delete

--- android/app/src/main/kotlin/com/example/api_demo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.api_demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utilis/endpointRes.dart
+++
@@ -1,6 +0,0 @@
-class EndPointRes {
-  static const baseUrl = "https://dummyjson.com/";
-  // End Point
-  static const productAPI = '${baseUrl}products';
-  static const addProductAPI = '${baseUrl}products/add';
-}
